c9b4b6f914ce88b3ba08873015ae8f6eb8e993e9
themellowj <54555664+themellowj@users.noreply.github.com>
2021-08-18 23:58:43 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImagesSource2 = "images/dice" + randomNumber2 + ".png";
+
+var images2 = document.querySelectorAll("img")[1];
+images2.setAttribute("src", randomImagesSource2);
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML= "Player 1 Wins";
+}
+
+else if(randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML= "Player 2 Wins";
+}
+else{
+  document.querySelector("h1").innerHTML= "Draw";
+}
